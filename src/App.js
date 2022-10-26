808c2816c3e5ca60543819735b9a17690f8a5258
Aravind S <aravindsputhoor@gmail.com>
2022-10-26 17:58:16 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,27 +1,43 @@
 import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import Counter from './components/Counter';
+import {useState} from 'react';
 
 function App() {
   const data = 'This is a data prop';
+  const [count, setCount] = useState(0);
+ 
+  const addCount = () => {
+    setCount(count+1);
+  }
+  let obj = {
+    title: 'First Counter',
+    count
+  }
   return (
     <div>
-      <Header data={data}/>
-       <h1 style={{ backgroundColor: 'red', color: 'white' }}>Hello World!</h1>
-       <Description/>
-       <Description/>
-       <Description/>
-       <Description/>
-       <p>{data}</p>
-       <Footer data={data}/>
+      <Header data={data} />
+      <h1 style={{ backgroundColor: 'red', color: 'white' }}>Hello World!</h1>
+      <Description />
+      <Description />
+      <Description />
+      <Description />
+      <p>{data}</p><br></br>
+
+      <Counter {...obj} />
+      <Counter title="Second Counter" count={count} />
+
+      <button onClick={addCount}>Add</button>
+      <Footer data={data} />
     </div>
   );
 }
 
 function Description() {
-  return(
+  return (
     <div>
-       <p className="hello">This is simple description</p><br></br>
+      <p className="hello">This is simple description</p><br></br>
     </div>
   );
 }
---
+++ src/components/Counter.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Counter({title, count}) {
+  // const {title, count} = props;
+  return (
+    <h1>{title}: {count} </h1>
+  )
+}
+
+export default Counter
