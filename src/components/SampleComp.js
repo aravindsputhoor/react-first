b41433388118d64e11e0044e7e6c6c33317d7077
Aravind S <aravindsputhoor@gmail.com>
2022-10-27 13:26:01 +0530

    useEffect

--- src/components/SampleComp.js
+++ src/components/SampleComp.js
@@ -1,7 +1,13 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const SampleComp = () => {
   const [count, setCount] = useState(0);
+  useEffect(()=>{
+    console.log('Mounting...');
+    return ()=>{
+      console.log('Unmounting...');
+    }
+  })
   return (
     <div>
       <h1>This is sample component : {count}</h1>
